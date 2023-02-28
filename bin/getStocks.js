7796ff3eefedf62cabac0128bdb795673d32e146
aliraza944 <alisabri022@gmail.com>
2023-02-28 23:07:11 +0500

    built js files

---
+++ bin/getStocks.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSingleStockInfo = void 0;
+const axios_1 = require("axios");
+const getSingleStockInfo = async (stock) => {
+    if (!stock) {
+        throw new Error("Stock symbol argument required");
+    }
+    if (typeof stock !== "string") {
+        throw new Error(`Invalid argument type for stock argument. Required: string. Found: ${typeof stock}`);
+    }
+    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${stock}`;
+    const res = await axios_1.default.get(url);
+    const { data } = res;
+    if (!data ||
+        !data.quoteResponse ||
+        !data.quoteResponse.result ||
+        data.quoteResponse.result.length === 0) {
+        throw new Error(`Error retrieving info for symbol ${stock}`);
+    }
+    const quoteResponse = data.quoteResponse.result[0];
+    return quoteResponse;
+};
+exports.getSingleStockInfo = getSingleStockInfo;
+//# sourceMappingURL=getStocks.js.map
--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,41 @@
 #!/usr/bin/env node
-const currentDateAndTime = new Date().toISOString();
-console.log(currentDateAndTime);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const getStocks_1 = require("./getStocks");
+const getVersion_1 = require("./getVersion");
+const getArgs = () => {
+    // We retrieve all the command arguments except the first 2
+    const args = process.argv.slice(2);
+    return args;
+};
+/**
+ * Command Help
+ */
+const printCommandHelp = () => {
+    const version = (0, getVersion_1.getVersion)();
+    const help = `
+pique-sous (version: ${version})
+
+A simple command to retrieve stock information.
+
+Example:
+
+$ ts-cli MSFT SFIX GOOG
+
+`;
+    console.log(help);
+};
+const symbols = getArgs();
+// Print help if no arguments
+if (symbols.length === 0) {
+    printCommandHelp();
+    (0, getVersion_1.getVersion)();
+    process.exit(0);
+}
+const now = new Date().toISOString();
+// Call the yahoo API for each symbol and display the result on the console
+symbols.forEach((symbol) => {
+    console.log(`Retrieving stock information for ${symbol} at date ${now}`);
+    (0, getStocks_1.getSingleStockInfo)(symbol).then(console.log);
+});
 //# sourceMappingURL=index.js.map
