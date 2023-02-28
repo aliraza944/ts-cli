07e5b8c96109770f582f3559934e044075eb9bdd
aliraza944 <alisabri022@gmail.com>
2023-02-28 22:43:43 +0500

    Added the methods to index and called dispaly methods to display on the cli

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,45 @@
 #!/usr/bin/env node
-const currentDateAndTime = new Date().toISOString();
 
-console.log(currentDateAndTime);
+import { getSingleStockInfo } from "./getStocks";
+import { getVersion } from "./getVersion";
+
+const getArgs = () => {
+  // We retrieve all the command arguments except the first 2
+  const args = process.argv.slice(2);
+  return args;
+};
+
+/**
+ * Command Help
+ */
+const printCommandHelp = () => {
+  const version = getVersion();
+  const help = `
+pique-sous (version: ${version})
+
+A simple command to retrieve stock information.
+
+Example:
+
+$ ts-cli MSFT SFIX GOOG
+
+`;
+  console.log(help);
+};
+
+const symbols = getArgs();
+
+// Print help if no arguments
+if (symbols.length === 0) {
+  printCommandHelp();
+  getVersion();
+  process.exit(0);
+}
+
+const now = new Date().toISOString();
+
+// Call the yahoo API for each symbol and display the result on the console
+symbols.forEach((symbol) => {
+  console.log(`Retrieving stock information for ${symbol} at date ${now}`);
+  getSingleStockInfo(symbol).then(console.log);
+});
